89d4c88bcb31ceffcafe61d157a1499a96ab8ab6
Tobias Kongsvik <tokongs98@gmail.com>
2020-08-27 00:08:17 +0200

    temp

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config({ path: `${__dirname}/.env` });
 
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const dbRouter = require('./routes/db');
 
@@ -21,9 +20,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/db', dbRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/db', dbRouter);
+
+// Serve frontend
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use((_, __, next) => {
